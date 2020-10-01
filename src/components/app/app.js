30dece777702fd8ec70b363a247a71b43543feb1
bonnysid <bonnysidworker@gmail.com>
2020-10-01 20:02:14 +0400

    add toggle btn for random character

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
+import './app.css';
 
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
-};
+export default class App extends Component {
 
-export default App;
+    constructor(props) {
+        super(props);
+        this.toggleRandomChar = this.toggleRandomChar.bind(this);
+    }
+
+    state = {
+        isClosed: false
+    }
+
+    toggleRandomChar() {
+        this.setState({isClosed: !this.state.isClosed});
+    }
+
+    render() {
+        const {isClosed} = this.state;
+        const randomChar = isClosed ? null : <RandomChar />; 
+        return (
+
+            <> 
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                            {randomChar}
+                            <div onClick={this.toggleRandomChar} className="btn-toggle">Toggle random Character</div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        )
+    }
+}
